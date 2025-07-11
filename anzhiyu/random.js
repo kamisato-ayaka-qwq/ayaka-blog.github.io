c954ead3fb87760f4bded771a956165b5ca96b71
kamisato-ayaka-qwq <3608088226@qq.com>
2025-07-11 21:39:34 +0800

    Site updated: 2025-07-11 21:39:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/10/hello-world/"];function toRandomPost(){
+var posts=["2025/07/11/Hello-World/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
